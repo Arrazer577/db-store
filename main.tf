01704a99a33e2e9e9c220914570c8dc40adb8eee
M4tr1x0x <alexgzzf@hotmail.com>
2024-10-28 23:30:14 -0600

    epic

---
+++ main.tf
@@ -0,0 +1,54 @@
+# Crear un par de claves SSH
+resource "aws_key_pair" "ultimatesecret_key" {
+  key_name   = "ultimatesecret"  
+  public_key = file("./secrets/ultimatesecret.pem.pub")  
+}
+
+
+resource "aws_security_group" "web_sg" {
+  name        = "allow_ssh_http"
+  description = "Permitir acceso SSH y HTTP"
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-085f9c64a9b75eed5"  
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.ultimatesecret_key.key_name  
+
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  tags = {
+    Name = "Terraform-WebServer"
+  }
+
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo yum update -y
+    sudo yum install -y nginx
+    sudo systemctl start nginx
+    sudo systemctl enable nginx
+  EOF
+}
